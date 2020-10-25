1d95767fa989ef5dd156e4a05571fdef01bb50bd
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-10-25 16:11:16 +0530

    Refactored: Code to get count from CSV Iterator

--- src/main/java/census/CensusAnalyser.java
+++ src/main/java/census/CensusAnalyser.java
@@ -14,11 +14,7 @@ public class CensusAnalyser {
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(csvFile));
 			Iterator<CSVStateCensus> censusIterator = this.getCSVFileIterator(reader, CSVStateCensus.class);
-			int countOfRecord = 0;
-			while (censusIterator.hasNext()) {
-				countOfRecord++;
-				CSVStateCensus censusData = censusIterator.next();
-			}
+			int countOfRecord = this.getCount(censusIterator);
 			return countOfRecord;
 		} catch (RuntimeException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.INCORRECT_FILE);
@@ -31,17 +27,22 @@ public class CensusAnalyser {
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(csvFile));
 			Iterator<CSVStateCode> censusIterator = this.getCSVFileIterator(reader, CSVStateCode.class);
-			int countOfRecord = 0;
-			while (censusIterator.hasNext()) {
-				countOfRecord++;
-				CSVStateCode censusData = censusIterator.next();
-			}
+			int countOfRecord = this.getCount(censusIterator);
 			return countOfRecord;
 		} catch (RuntimeException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.INCORRECT_FILE);
 		} catch (NoSuchFileException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.NO_FILE);
 		}
+	}
+	
+	private <E> int getCount(Iterator<E> iterator) {
+		int countOfRecord = 0;
+		while (iterator.hasNext()) {
+			countOfRecord++;
+			E censusData = iterator.next();
+		}
+		return countOfRecord;
 	}
 	
 	private <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
